6714493e6aecbb488f63fc600548000a8eaca432
Ondrej Drapalik <ondrej.drapalik@gmail.com>
2021-05-03 16:03:36 +0200

    last version before adding buttons

--- app.js
+++ app.js
@@ -5,19 +5,9 @@ let myText = 'I am a string';
 let newString = myText.replace('string', 'sausage');
 console.log(newString);
 
-// function that plays single round - takes player and computer play functions as a parameters 
-    // playerSelection function
-        // prompt case insensitive input
-        //     check whether word is "rock" "paper" or "scissor" otherwise input or exit
-        // return value
-    // computerSlelection function
-        // return random from rock paper scissors
-    // if statement comparing player values to computers
-        // if paylerSelection === "rock" AND computerSelection === "paper" –> you lose
-        // ...
 
 function game() {
-    let choices = ["rock","paper","scissors"]
+    let choices = ["rock","paper","scissors"];
     const playerSelection = playerInput();
     const computerSelection = computerPlay();
     
@@ -40,7 +30,9 @@ function game() {
     }
     
     
-    
+    // If I devide it to 3 fuctions - computerWin a playerWin + draw -> it will be better for
+    //         counding the score
+
     function playRound(playerSelection, computerSelection) {
         if (playerSelection === "rock" && computerSelection === "rock") {
             return("It's a draw.");
@@ -71,6 +63,18 @@ function game() {
     return(playRound(playerSelection, computerSelection)); 
 }
 
-console.log(game());
-console.log(game());
-console.log(game());
+for (let i=0; i<5; i++) {
+    console.log(game());
+}
+
+// // or while loop
+
+// let i = 0;
+// while (i<3) {
+//     console.log(game());
+//     i++;
+// }
+
+// // or different while loop
+// let i=3;
+// while(i) console.log(game())(i--);
---
+++ notes.md
@@ -0,0 +1,12 @@
+Here is a first note!
+
+function that plays single round - takes player and computer play functions as a parameters 
+    playerSelection function
+        prompt case insensitive input
+            check whether word is "rock" "paper" or "scissor" otherwise input or exit
+        return value
+    computerSlelection function
+        return random from rock paper scissors
+    if statement comparing player values to computers
+        if paylerSelection === "rock" AND computerSelection === "paper" –> you lose
+        ...
